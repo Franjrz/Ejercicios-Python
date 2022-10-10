e10a0e1db35bcd4c2bf2223a2687ad48c2ee3bbe
narfdf999 <81768322+Franjrz@users.noreply.github.com>
2022-10-10 17:03:11 +0200

    Ejercicios

--- Intermedio/Codificador-romano.py
+++ Intermedio/Codificador-romano.py
@@ -1,15 +1,15 @@
 a = [['I','V'],['X','L'],['C','D'],['M']]
 def solution(n):
-    sol = ""
+    solu = ""
     n = [int(d) for d in str(n)]
     n = n[::-1]
     for i in range(len(n)):
         if n[i] < 4:
-            sol = a[i][0]*n[i] + sol
+            solu = a[i][0]*n[i] + solu
         elif n[i] == 4:
-            sol = a[i][0]+a[i][1] + sol
+            solu = a[i][0]+a[i][1] + solu
         elif n[i] < 9:
-            sol = a[i][1]+a[i][0]*(n[i]-5) + sol
+            solu = a[i][1]+a[i][0]*(n[i]-5) + solu
         elif n[i] == 9:
-            sol = a[i][0]+a[i+1][0] + sol
-    return sol
+            solu = a[i][0]+a[i+1][0] + solu
+    return solu
